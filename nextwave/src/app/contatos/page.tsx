a8bad6f1738579de2d6c72ca230ce35fdd91e4f2
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-08-28 23:42:45 -0300

    Começando..

---
+++ nextwave/src/app/contatos/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Contatos(){
+    return(
+        <div>
+            <h1>Oi</h1>
+        </div>
+    )
+}
---
+++ nextwave/src/components/header/index.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+
+export function Header() {
+    return(
+        <header className="flex px-2 py-4 bg-zinc-900 text-white">
+            <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
+                <div>
+                    Teste
+                </div>
+
+                <nav>
+                    <ul className="flex items-center justify-center gap-2">
+                        <li>
+                            <Link href="/contatos">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/">
+                                Posts
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
